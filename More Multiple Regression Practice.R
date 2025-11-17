7cfe9673534069a93500a9530461dce414a0dc4b
sonackumar-oss <sona.c.kumar@gmail.com>
2025-11-17 11:30:36 -0500

    Add files via upload

---
+++ More Multiple Regression Practice.R
@@ -0,0 +1,10 @@
+# The Music and Movies dataset includes people’s ratings of different genres of music and different types of movies (on 1-5 liking scales), along with a bunch of personality-related statements (on 1-5 agreement scales). 
+
+# Imagine we want to know what factors predict people's movie tastes. 
+# We will use linear regression to model people’s taste in movies.
+
+# Conduct a linear regression to determine whether liking of Horror is predicted by agreement with the following two statements:
+# -	I often think about and regret the decisions I make.
+# -	I don’t like seeing animals suffering.
+
+# Using what we've talked about in class, conduct a multiple regression examining this question.
